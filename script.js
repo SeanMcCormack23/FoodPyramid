2c1f3708a1422d85d06c35281bec9d934188df07
Sean McCormack <73062492+SeanMcCormack23@users.noreply.github.com>
2022-01-06 22:18:51 +0000

    height changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function decrease() {
+function decrease1() {
   trapHeight = 
   getComputedStyle(document.documentElement).getPropertyValue('--trap-height');
   document.documentElement.style.setProperty('--trap-height',(parseInt(trapHeight,10)-10)+"px");
 }
+// function decrease2() {
+//   trapHeight = 
+//   getComputedStyle(document.documentElement).getPropertyValue('--trap-height');
+//   document.documentElement.style.setProperty('--trap-height',(parseInt(trapHeight,10)-10)+"px");
+// }
